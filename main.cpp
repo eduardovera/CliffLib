ef8bda29130d11ae58082757de2814ccf852fe5b
Eduardo Vera Sousa <eduardoverasousa@outlook.com>
2016-09-12 18:14:06 -0300

    Added 'print()' function to multivector

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
-
+#include <multivector.h>
 using namespace std;
 
-int main(int argc, char *argv[])
-{
-    cout << "Hello World!" << endl;
-    return 0;
+int main(int argc, char *argv[]) {
+
+    multivector M({0.0, -3.0, 2.5, -4.0, 5.0, -9.0, 4.2, -1.3, 8.0});
+    M.print();
+
 }
--- multivector.h
+++ multivector.h
@@ -1,4 +1,54 @@
 #ifndef MULTIVECTOR_H
 #define MULTIVECTOR_H
 
+#include <iostream>
+#include <map>
+#include <bitset>
+#include <algorithm>
+
+using namespace std;
+
+class multivector {
+
+    private:
+        std::map<uint, double> M;
+
+        string get_base(int index) {
+            std::string binary = std::bitset<32>(index).to_string();
+            reverse(binary.begin(), binary.end());
+            string ret = "";
+            string s = "1";
+
+            size_t pos = binary.find(s);
+            while(pos != string::npos) {
+                ret.append("e");
+                ret.append(to_string(pos + 1));
+                pos = binary.find(s, pos+1);
+                if (pos != string::npos) {
+                    ret.append("^");
+                }
+            }
+            return ret;
+        }
+
+    public:
+
+        multivector(std::initializer_list<double> l) {
+            int index = 0;
+            for (std::initializer_list<double>::iterator it = l.begin(); it != l.end(); ++it) {
+                if ((*it) != 0.0) {
+                    M[index] = (*it);
+                }
+                index++;
+            }
+        }
+
+        void print() {
+            for (std::map<uint, double>::iterator it = M.begin(); it != M.end(); ++it) {
+                cout << ((*it).second > 0.0 ? "+" : "") << (*it).second << "(" << get_base((*it).first) << ")";
+            }
+            cout << endl;
+        }
+};
+
 #endif // MULTIVECTOR_H
