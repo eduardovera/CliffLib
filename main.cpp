18055ee5acbe9fcef699c8b692a83ec9af15977c
Eduardo Vera Sousa <Eduardo Vera Sousa>
2016-09-30 01:37:02 -0300

    Fixed bug

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
 
-    multivector<double> M = e(4) ^ e(1) ^ e(2) ^ e(3) ^ e(5) ^ e(6);
+    multivector<double> M = 5 + e(1) ^ e(2);
     cout << M << endl;
 
 }
